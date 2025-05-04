edad5dfaa818c132d8f2318a66c6ac8ebd4740a7
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-05-04 12:00:37 -0400

    Adding Modal Window Functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModalButton = document.querySelector('.close-modal')
+const openModalButtons = document.querySelectorAll('.show-modal')
+
+
+
+
+//Functions to Open and Close Windows 
+const openWindow = () =>{
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeWindow = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+//For Loop to Add Event Listeners for Modals 
+for(const element of openModalButtons){
+  element.addEventListener('click', openWindow) 
+  closeModalButton.addEventListener('click', closeWindow)
+}
+
+//Event Listener for Key Presses 
+document.addEventListener('keydown', function(e){
+  console.log(e)
+  if(!modal.classList.contains('hidden') && e.key === 'Escape') closeWindow()
+})
